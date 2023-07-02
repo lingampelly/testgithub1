498523ee8e3919c8d1262bd38c6b0477c8b7baac
santhosh <santhoshk621@gmail.com>
2023-07-02 14:20:45 +0530

    ghghgijnijjh

--- src/dempgit/demogit.java
+++ src/dempgit/demogit.java
@@ -51,6 +51,24 @@ public class demogit {
 		System.out.println("hrllo eorld");
 		System.out.println("dhdshhshdh");
 
+		int y=2;
+		try
+		{		int z=9/0;
+
+		//throw New ArithamaticException();
+		}
+		catch(Exception e) {
+
+			System.out.println(e.getMessage());
+		System.out.println(e.toString());
+		}
+		catch(NoClassDefFoundError e) {
+			
+			System.out.println(e);
+		}
+
+	
+
 		System.out.println("hrllo eorld");
 		System.out.println("dhdshhshdh");System.out.println("hrllo eorld");
 		System.out.println("dhdshhshdh");
