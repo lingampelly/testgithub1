af5594a71e6be7eb4eb210775c7b8153b2a08b4c
santhosh <santhoshk621@gmail.com>
2023-07-02 13:51:30 +0530

    second commit

--- src/dempgit/demogit.java
+++ src/dempgit/demogit.java
@@ -4,8 +4,14 @@ public class demogit {
 
 	public static void main(String[] args) {
 		
+	
 		System.out.println("hrllo eorld");
+		System.out.println("dhdshhshdh");System.out.println("hrllo eorld");
 		System.out.println("dhdshhshdh");
+		
+		System.out.println("hrllo eorld");
+		System.out.println("dhdshhshdh");	
+		
 		// TODO Auto-generated method stub
 
 	}
