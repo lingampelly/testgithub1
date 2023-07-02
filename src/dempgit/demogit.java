f3c75f3ae6028f54b84f3c7d8916405d3c0e4c36
santhosh <santhoshk621@gmail.com>
2023-07-02 13:44:22 +0530

    commit the firt messhagfe

---
+++ src/dempgit/demogit.java
@@ -0,0 +1,13 @@
+package dempgit;
+
+public class demogit {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hrllo eorld");
+		System.out.println("dhdshhshdh");
+		// TODO Auto-generated method stub
+
+	}
+
+}
